0bb5b45e3e68f51f94bcdd2b4f2002a9292bd5f0
Alexander Malashkevich <62018835+MrNeelixs@users.noreply.github.com>
2020-08-30 07:31:48 +0300

    Task for lesson number 2

---
+++ src/main/java/org/example/City.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class City {
+    private int population ;    // приватное целочисленное поле
+    private String name ;       // приватное строковое поле
+    public static final long MAX_NUMBER_OF_PEOPLE = 2000000;    // публичное константное статическое длинночисленное (Long) поле
+
+    public City (int cityPopulation, String cityName) {     // Кастомный конструктор
+        this.population = cityPopulation ;
+        this.name = cityName ;
+    }
+
+    public City () {
+        // Пустой Конструктор
+    }
+
+    public void printInfoCity () {   // Вывод информации
+        System.out.println("City information");
+        System.out.println("The city is called: " + name);
+        System.out.println("Lives in the city: " + population);
+        System.out.println("Maximum inhabitants in a city: " + MAX_NUMBER_OF_PEOPLE);
+
+    }
+}
+// by Mr.Neelixs
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        City javaCity = new City(23 , "JavaBLRGroup" ) ;    // Вызов класса
+
+
+
+        System.out.println("--------------------------------------");    // Вывод информации
+        javaCity.printInfoCity();                                        // Вызов метода из класса City
+        System.out.println("--------------------------------------");
+
     }
 }
