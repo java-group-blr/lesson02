e4dae5d18ab24118213e822c22c782a25faf34d8
Alexander Malashkevich <62018835+MrNeelixs@users.noreply.github.com>
2020-08-30 08:04:16 +0300

    Task for lesson number 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        City javaCity = new City(23 , "JavaBLRGroup" ) ;    // Вызов класса
-
+        City castCity = new City() ;
+        City javaCity = new City(5 , "JavaMr.Neelixs" ) ;    // Вызов класса
 
 
         System.out.println("--------------------------------------");    // Вывод информации
         javaCity.printInfoCity();                                        // Вызов метода из класса City
+        System.out.println("--------------------------------------");
+        castCity.printInfoCity();
         System.out.println("--------------------------------------");
 
     }
