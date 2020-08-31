0a56c6c602534cb47180d5e84c338e4821cf4e43
Angela-h-89 <angela-h-89@yandex.by>
2020-08-31 23:03:50 +0300

    Homework_City_Angela

---
+++ City.java
@@ -0,0 +1,23 @@
+public class City {
+    private int population;
+    private String name;
+    public static final long max_number_of_people=9000000;
+
+    public City(int myPopulation, String myName) {
+        this.population=myPopulation;
+        this.name=myName;
+    }
+
+    public City(){
+        this.population=400000;
+        this.name="Brest";
+    }
+
+    public void printInfo(){
+        System.out.println("City information");
+        System.out.println("City name: "+name);
+        System.out.println("City population: "+population);
+        System.out.println("Maximum number of people in the city: "+max_number_of_people);
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String[] args){
+        City myCity=new City();
+        City infoCity = new City(2000000, "Minsk");
+
+        myCity.printInfo();
+        System.out.println("---------------------------------------------");
+        infoCity.printInfo();
+    }
+}
