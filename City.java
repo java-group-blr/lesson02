abc724d1fffb2b3c39ead12ee83ac78124119d1e
di-menchy <70278272+di-menchy@users.noreply.github.com>
2020-09-01 20:52:02 +0200

    Update City.java

--- City.java
+++ City.java
@@ -3,23 +3,19 @@ package com.company;
 public class City {
     private int population;
     private String name;
-    public static final Long MAX_NUMBER_OF_PEOPLE = 5_000_000L;
+    public static final Long MAX_NUMBER_OF_PEOPLE = 500_000L;
 
-    public City(int myPopulation, String myName) {
-        this.population = myPopulation;
-        this.name = myName;
+    public City(int population, String name) {
+        this.population = population;
+        this.name = name;
     }
 
     public City() {
-        this.population = 60_000;
         this.name = "Muhosransk";
+        this.population = 60_000;
     }
 
     public void printInfo() {
-        System.out.println("Information about the City ");
-        System.out.println("My City population is " + population);
-        System.out.println("The maximum population of my City is: " + MAX_NUMBER_OF_PEOPLE);
-        System.out.println("The name of my City is: " + name);
-
+        System.out.println("name+ ", " + population + MAX_NUMBER_OF_PEOPLE);
     }
 }
