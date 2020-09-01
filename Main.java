a38ad00d835a41666b242098e2d16efe6ef0a4b9
di-menchy <70278272+di-menchy@users.noreply.github.com>
2020-09-01 20:44:29 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
     public static void main (String[] args) {
         City myCity = new City();
-        City infoCity = new City(15000, "Lol" );
+        City infoCity = new City(15_000, "Lol" );
 
         myCity.printInfo();
         System.out.println("----------------------------------");
