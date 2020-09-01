1f3ee9110e23d9472d01b72e76a486b65143ec74
di-menchy <70278272+di-menchy@users.noreply.github.com>
2020-09-01 22:43:07 +0200

    Update City.java

--- src/main/java/org/example/City.java
+++ src/main/java/org/example/City.java
@@ -1,25 +1,24 @@
-package com.company;
+package org.example;
 
 public class City {
-    private final int population;
-    private final String name;
-    public static final Long MAX_NUMBER_OF_PEOPLE= 5000000L;
+    private int population;
+    private String name;
+    public static final Long MAX_NUMBER_OF_PEOPLE = 5000000L;
 
     public City() {
         this.population = 60000;
         this.name = "Muhosransk";
     }
     
-    public City(int myPopulation, String myName) {
-        this.population = myPopulation;
-        this.name = myName;
+    public City(int population, String name) {
+        this.population = population;
+        this.name = name;
     }
     
     public void printInfo() {
         System.out.println("Information about the City ");
-        System.out.println("My City population is " + population);
-        System.out.println("The maximum population of my City is: " + MAX_NUMBER_OF_PEOPLE);
+        System.out.println("My City population is " + population + "ppl");
         System.out.println("The name of my City is: " + name);
-
     }
+    
 }
