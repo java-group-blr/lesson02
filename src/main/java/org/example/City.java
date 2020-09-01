7d743eb918e639cbe15218a8722acebe1601d6c6
RomarioAgro89 <romio.kampotti@gmail.com>
2020-09-02 00:43:02 +0300

    Task2

---
+++ src/main/java/org/example/City.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class City {
+
+    private int population;
+    public String name;
+    public static final Long MAX_NUMBER_OF_PEOPLE = 50000000L;
+
+    public City(int population, String name) {
+
+        this.population = population;
+        this.name = name;
+
+    }
+
+    public City (){
+        this.population = 4600000;
+        this.name = "Tokyo";
+
+    }
+    public void printInfo (){
+        System.out.println ("My car can have this amount of people at once: " + population);
+        System.out.println ("My car has a wheel: " + MAX_NUMBER_OF_PEOPLE);
+        System.out.println ("Color of my car is: " + name);
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        City myCity = new City ();
+        City myCity2 = new City(40000000, "Tokyo");
+
+        myCity.printInfo();
+        System.out.println("-------------------------------------");
+        myCity2.printInfo();
     }
 }
