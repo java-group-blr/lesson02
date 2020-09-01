f357fdd6ed2d1931f5a2ddb214a826de02fc08f5
FEDIA <belife190689@gmail.com>
2020-09-02 00:03:40 +0300

    kill

---
+++ src/main/java/org/example/City.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class City {
+    private int population;
+    private String name;
+    public static final Long MAX_NUMBER_OF_PEOPLE = 750000L;
+
+
+
+    public City(int population, String name) {
+        this.population = population;
+        this.name = name;
+
+    }
+
+    public City() {
+        this.population = 510000;
+        this.name = "Гомель";
+
+    }
+
+    public void printOnDisplay() {
+        System.out.println("Информация: ");
+        System.out.println("Мой город " + name + ", в нем проживает - " + population + " человек.");
+        System.out.println("Максимальновозможное количество - " + MAX_NUMBER_OF_PEOPLE +" жителей.");
+    }
+
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        City c1= new City();
+        c1.printOnDisplay();
+
+
     }
+
 }
