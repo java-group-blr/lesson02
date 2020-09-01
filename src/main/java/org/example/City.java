a1b5c6ad07b73c8529c5c3c94051d46b6a7f3c4e
di-menchy <123twistozz@gmail.com>
2020-09-01 22:11:09 +0200

    javaPoshadi

---
+++ src/main/java/org/example/City.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class City {
+    private int population;
+    private String name;
+    public static final Long MAX_NUMBER_OF_PEOPLE= 5000000L;
+
+    public City(int population, String name) {
+        this.population = population;
+        this.name = name;
+    }
+
+    public City() {
+        this.name = "Muhosransk";
+        this.population = 60000;
+    }
+
+    public void printInfo() {
+        System.out.println(name+ ", " +population + MAX_NUMBER_OF_PEOPLE);
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
-package org.example;
+package com.company;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main (String[] args) {
+        City myCity = new City();
+        City infoCity = new City(15000, "Grez" );
+
+        myCity.printInfo();
+        System.out.println("----------------------------------");
+        infoCity.printInfo();
     }
 }
