11d410450de3c07c1718c6eabbca4e273ca3c492
mmalashchonak <mmalashchonak@gmail.com>
2020-09-01 17:47:54 +0300

    New class City

---
+++ src/main/java/org/example/City.java
@@ -0,0 +1,22 @@
+package main.java.org.example;
+
+public class City {
+    private int population;
+
+    private String name;
+    public static final Long MAX_NUMBER_OF_PEOPLE = 10_000_000L;
+
+    City() {
+        this.population = 2_000_000;
+        this.name = "Minsk";
+    }
+
+    City(int population, String name) {
+        this.population = population;
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("Welcome to " + name);
+    }
+}
