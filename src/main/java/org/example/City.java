1739e0be0e15eb1ad8975eba6908c4d0640ae19e
art-orient <art-orient@tut.by>
2020-09-02 08:03:37 +0300

    Исправление

--- src/main/java/org/example/City.java
+++ src/main/java/org/example/City.java
@@ -1,22 +1,23 @@
 package org.example;
 
-    public City () {
-        this.population = 400000L;
-        this.name = "Слуцк";
-    }
-
-    public City (int population, String name) {
-        this.population = population;
-        this.name = name;
-    }
-
     public class City {
         private int population;
         private String name;
         public static final Long MAX_NUMBER_OF_PEOPLE = 2000000L;
 
         public void printInfo () {
-            System.out.println("В городе " + this.name + "жыве " + this.population + "невероятных чнловек");
+            System.out.println("В городе " + this.name + " жыве " + this.population + " невероятных чнловек");
+            System.out.println("А когда-то было " + MAX_NUMBER_OF_PEOPLE + " человек");
+        }
+
+        public City () {
+            this.population = 1600000;
+            this.name = "Минск";
+        }
+
+        public City (int population, String name) {
+            this.population = population;
+            this.name = name;
         }
     }
 
