939c79fbc9b7aebea760f9285e6aae9bc77f8791
nickisdst <nickisdst@gmail.com>
2020-09-02 14:21:20 +0300

    remake

--- src/main/java/org/example/City.java
+++ src/main/java/org/example/City.java
@@ -6,17 +6,17 @@ public class City {
     private static final long MAX_NUMBER_OF_PEOPLE = 3222280;
     private String name;
 
-    public void printInfo(){
-        System.out.println("Название города: " + name + "\n" + "Население: " + population + "\n" + "Максимальное население: " + MAX_NUMBER_OF_PEOPLE);
-    }
-
-    public City(String name, int population){
-        this.name = name;
+    public City(int population, String name){
         this.population = population;
+        this.name = name;
     }
 
     public City(){
-        this.name = "No Name";
         this.population = 322228;
+        this.name = "No Name";
+    }
+
+    public void printInfo(){
+        System.out.println("Название города: " + name + "\n" + "Население: " + population + "\n" + "Максимальное население: " + MAX_NUMBER_OF_PEOPLE);
     }
 }
